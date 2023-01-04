dccdc060523fd5eb8158841beee354b9b9d18a99
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2023-01-04 18:54:23 -0300

    atualizado método AdicionarUsuário e ListarContas

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -1,24 +1,25 @@
 ﻿using System;
 using System.Net.Mail;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ByteBank
 {
     public class Program
     {
-
-        static void showMenu()
+        public static void MenuPrincipal()
         {
-            Console.WriteLine("O que você precisa?");
-            Console.WriteLine("1. Adicionar nova conta/usuário");
+            Console.WriteLine("O que você precisa?\n");
+            Console.WriteLine("1. Adicionar nova conta");
             Console.WriteLine("2. Listar todas as contas registradas");
-            Console.WriteLine("3. Acessar uma conta");
+            Console.WriteLine("3. Acessar/Manipular uma conta");
             Console.WriteLine("4. Excluir uma conta");
-            Console.WriteLine("0. Finalizar programa\n");
+            Console.WriteLine("5. Finalizar programa\n");
             Console.Write("Digite a opção desejada: ");
             
         }
 
-        static void RegistrarNovoUsuario(List<string> titulares, List<string> cpfs, List<string> chavesPix)
+
+        public static void RegistrarNovoUsuario(List<string> titulares, List<string> cpfs, List<string> chavesPix, List<string> senhas, List<int> contas)
         {
             Console.Write("Digite o nome completo: ");
             titulares.Add(Console.ReadLine());
@@ -26,42 +27,78 @@ namespace ByteBank
             cpfs.Add(Console.ReadLine());
             Console.Write("Adicione uma chave Pix (uma palavra ou email): ");
             chavesPix.Add(Console.ReadLine());
+            Console.Write("Digite uma nova senha (4 a 6 caracteres): ");
+            senhas.Add(Console.ReadLine());
             Console.WriteLine();
-            
+            //Console.Write("Repita a nova senha: ");
+            //string entrada = Console.ReadLine();
+
+            Random numeroAleatorio = new Random();
+            int conta = numeroAleatorio.Next(2500, 8750);
+            contas.Add(conta);
+
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Conta nº {0} adicionada com sucesso.\n", conta);
+            Console.ResetColor();
         }
-        private static void RegistrarSenhas()
+        
+        public static string EsconderSenha()
         {
-            
+            var pass = string.Empty;
+            ConsoleKey key;
+
+            do
+            {
+                var keyInfo = Console.ReadKey(intercept: true);
+                key = keyInfo.Key;
+
+                if (key == ConsoleKey.Backspace && pass.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    pass = pass[0..^1];
+                }
+                else if (!char.IsControl(keyInfo.KeyChar))
+                {
+                    Console.Write("*");
+                    pass += keyInfo.KeyChar;
+                }
+            } while (key != ConsoleKey.Enter);
+
+            Console.WriteLine();
+            return pass;
         }
-        static void ListarTodasAsContas(List<string> titulares, List<string> cpfs, List<string> chavesPix)
+
+        public static void ListarTodasAsContas(List<int> contas, List<string> titulares, List<string> cpfs, List<string> chavesPix)
         {
             for(int i = 0; i < cpfs.Count; i++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkGreen;
-                Console.WriteLine($"Titular: {titulares[i]} | CPF: {cpfs[i]} | Chave Pix: {chavesPix[i]}\n");
+
+                Console.WriteLine($"Conta: {contas[i]} | Titular: {titulares[i]} | CPF: {cpfs[i]} | Chave Pix: {chavesPix[i]}\n");
                 Console.ResetColor();
                 
             }
         }
+
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
             Console.WriteLine("Olá! Bem-Vindo(a) ao ByteBank!\n");
             Console.ResetColor();
-            
+
+            List<int> contas = new List<int>();
             List<string> titulares = new List<string>();
             List<string> cpfs = new List<string>();
             List<string> chavesPix = new List<string>();
-
-            string[] senhas = new string[2];
+            List<string> senhas = new List<string>();
             
-            Console.WriteLine() ;
+            Console.WriteLine();
 
             int opcao;
 
             do
             {
-                showMenu();
+                MenuPrincipal();
                 opcao = int.Parse(Console.ReadLine());
 
                 Console.WriteLine();
@@ -70,23 +107,31 @@ namespace ByteBank
 
                 switch (opcao)
                 {
-                    case 0: 
-                        Console.WriteLine("Finalizando o programa... Até mais!");
-                        break;
                     case 1:
-                        RegistrarNovoUsuario(titulares, cpfs, chavesPix);
-                        RegistrarSenhas(senhas);
+                        RegistrarNovoUsuario(titulares, cpfs, chavesPix, senhas, contas);
+                        //RegistrarSenhas(senhas);
                         break;
                     case 2:
-                        ListarTodasAsContas(titulares, cpfs, chavesPix);    
+                        ListarTodasAsContas(contas, titulares, cpfs, chavesPix);    
                         break;
-                    
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        Console.WriteLine("Finalizando o programa... Até mais!");
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.DarkYellow;
+                        Console.WriteLine("Opção inválida. Por favor, tente novamente.");
+                        Console.ResetColor();
+                        break;
 
 
                 }
                                 
             }
-            while (opcao != 0);
+            while (opcao != 5);
             
 
 
