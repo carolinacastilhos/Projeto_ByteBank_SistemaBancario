bdf19d81f6ca8261a692d8348a04e7fef974ee63
Carolina Castilhos <carolinacastilhos.vet@gmail.com>
2023-01-05 02:05:15 -0300

    adicionado método ExluirConta e iniciado MenuManipularContar

--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -18,6 +18,15 @@ namespace ByteBank
             
         }
 
+        public static void MenuManipularConta()
+        {
+            Console.WriteLine("O que você precisa?\n");
+            Console.WriteLine("1. Saldo da conta");
+            Console.WriteLine("2. Detalhes da conta");
+            Console.WriteLine("3. Realizar transações");
+            Console.WriteLine("4. Sair da conta (Logout)\n");
+            Console.Write("Digite a opção desejada: ");
+        }
 
         public static void RegistrarNovoUsuario(List<string> titulares, List<string> cpfs, List<string> chavesPix, List<string> senhas, List<int> contas)
         {
@@ -27,18 +36,39 @@ namespace ByteBank
             cpfs.Add(Console.ReadLine());
             Console.Write("Adicione uma chave Pix (uma palavra ou email): ");
             chavesPix.Add(Console.ReadLine());
-            Console.Write("Digite uma nova senha (4 a 6 caracteres): ");
-            senhas.Add(Console.ReadLine());
-            Console.WriteLine();
-            //Console.Write("Repita a nova senha: ");
-            //string entrada = Console.ReadLine();
+
+            string senha = "0";
+            string senha2 = "1";
+
+            do
+            {
+                Console.Write("Digite uma nova senha: ");
+                senha = EsconderSenha();
+                Console.Write("Repita a nova senha: ");
+                senha2 = EsconderSenha();
+                
+                if (senha != senha2)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("As senhas não conferem. Por favor, digite novamente.\n");
+                    Console.ResetColor();
+                }
+            }
+            while (senha != senha2);
+
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Senha salva.");
+            Console.ResetColor();
+            
+            senhas.Add(senha);
 
             Random numeroAleatorio = new Random();
             int conta = numeroAleatorio.Next(2500, 8750);
             contas.Add(conta);
 
+            
             Console.ForegroundColor= ConsoleColor.Green;
-            Console.WriteLine("Conta nº {0} adicionada com sucesso.\n", conta);
+            Console.WriteLine("\nConta nº {0} adicionada com sucesso.\n", conta);
             Console.ResetColor();
         }
         
@@ -80,6 +110,62 @@ namespace ByteBank
             }
         }
 
+        public static void LoginDeAcesso(List<string> cpfs, List<string> senhas)
+        {
+            Console.WriteLine("Login de Acesso");
+            Console.WriteLine("...............\n");
+
+            int indexParaLogar;
+            int indexSenhaLogin;
+
+            do
+            {
+                Console.Write("Digite o CPF: ");
+                string cpfParaLogar = Console.ReadLine();
+                indexParaLogar = cpfs.FindIndex(cpf => cpf == cpfParaLogar);
+                Console.Write("Digite a senha: ");
+                string senhaLogin = EsconderSenha();
+                indexSenhaLogin = senhas.FindIndex(s => s == senhaLogin);
+
+                if (indexParaLogar == -1 || indexSenhaLogin == -1)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("\nNão foi possível acessar a conta.\n" +
+                        "CPF ou senha incorretos. Por favor, tente novamente.\n");
+                    Console.WriteLine();
+                    Console.ResetColor();
+                }
+                                                                               
+            }
+            while (indexParaLogar != -1 && indexSenhaLogin != -1); //NÃO ESTÁ FAZENDO O CICLO CORRETAMENTE SE ERRA O CPF E/OU SENHA. CORRIGIR!
+            
+            
+        }
+                
+        public static void ExcluirConta(List<string> cpfs, List<string> titulares, List<string> chavesPix, List<string> senhas)
+        {
+            Console.Write("Digite o CPF: ");
+            string cpfParaDeletar = Console.ReadLine();
+            int indexParaDeletar = cpfs.FindIndex(cpf => cpf == cpfParaDeletar);
+
+            if (indexParaDeletar == -1)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Não foi possível deletar esta conta");
+                Console.WriteLine("MOTIVO: Conta não encontrada.");
+                Console.ResetColor();
+            }
+
+            cpfs.Remove(cpfParaDeletar);
+            titulares.RemoveAt(indexParaDeletar);
+            senhas.RemoveAt(indexParaDeletar);
+            chavesPix.RemoveAt(indexParaDeletar);
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nConta deletada com sucesso.\n");
+            Console.ResetColor();
+        }
+
         public static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.DarkCyan;
@@ -109,14 +195,17 @@ namespace ByteBank
                 {
                     case 1:
                         RegistrarNovoUsuario(titulares, cpfs, chavesPix, senhas, contas);
-                        //RegistrarSenhas(senhas);
                         break;
                     case 2:
                         ListarTodasAsContas(contas, titulares, cpfs, chavesPix);    
                         break;
                     case 3:
+                        LoginDeAcesso(cpfs, senhas);
+                        MenuManipularConta();
+
                         break;
                     case 4:
+                        ExcluirConta(cpfs, titulares, chavesPix, senhas);
                         break;
                     case 5:
                         Console.WriteLine("Finalizando o programa... Até mais!");
@@ -132,13 +221,11 @@ namespace ByteBank
                                 
             }
             while (opcao != 5);
+
             
 
 
-
         }
-
-        
+                
     }
-    
 }
